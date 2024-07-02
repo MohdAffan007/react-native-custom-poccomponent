532bb3dc9d0554ee8025b308758bcbfdbfb0e0d9
MohdAffan007 <affan.taj@gmail.com>
2024-07-02 15:07:19 +0530

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,18 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
+// export function multiply(a: number, b: number): Promise<number> {
+//   return Promise.resolve(a * b);
+// }
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './navigation/Routes';
+// import 'react-native-gesture-handler';
+
+const POCComponent = () => {
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  );
+};
+
+export default POCComponent;
---
+++ src/navigation/Routes.js
@@ -0,0 +1,40 @@
+// AppNavigator.js
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ChooseYourPlan from '../screens/ChooseYourPlan';
+import { Button, Image, Text,  BackHandler, TouchableOpacity, NativeModules } from 'react-native';
+import SelectPlans from '../screens/SelectPlans';
+
+const Stack = createNativeStackNavigator();
+const handleBackButton = () => {
+  // BackHandler.exitApp(); 
+};
+const {TestConnectNative} = NativeModules
+
+const Routes = ({rootTag}) => {
+  return (
+    <Stack.Navigator >
+      <Stack.Screen name="ChooseYourPlan" component={ChooseYourPlan}  options={{
+     headerTitle: 'Choose a plan to watch',
+          headerLeft: () => (
+           <>
+           <TouchableOpacity onPress={()=>{
+          TestConnectNative?.dismissViewController?.(rootTag)
+        }    }>
+           <Image
+                source={require('../assets/png/backArrowPng.png')}
+                style={{ width: 25, height: 25, marginRight: 10}}
+              />
+           </TouchableOpacity>
+           </>
+          ),
+        }}/>
+      <Stack.Screen initialParams={{rootTag}} name="SelectPlans" component={SelectPlans} options={{
+     headerTitle: 'Click to choose any 6 Apps',
+
+        }}/>
+    </Stack.Navigator>
+  );
+};
+
+export default Routes;
